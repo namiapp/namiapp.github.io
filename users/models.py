89e1c26e17b51d5c40a22858f43b51fd0b2fef25
namiapp <fortyninechickenfingers@gmail.com>
2018-05-30 01:59:27 +0900

    Built custom user models, but migration inconsistency is preventing me from migrating.

--- users/models.py
+++ users/models.py
@@ -10,37 +10,3 @@ class CustomUser(AbstractUser):
 
 	def __str__(self):
 		return self.email
-
-# # Universities model
-# class University(models.Model):
-
-# 	name    = models.CharField(max_length=200)
-# 	email   = models.EmailField()
-# 	address = models.CharField(max_length=50)
-# 	country = models.CharField(max_length=50)
-
-# 	# many users to many universities
-# 	# student    = models.ManyToManyField('Student')
-
-# 	# Meta's verbose_name_plural pluralizes universities correctly
-# 	class Meta:
-# 		verbose_name = "University"
-# 		verbose_name_plural = "Universities"
-
-
-# 	def __str__(self):
-# 		return self.name
-
-# # a university can have multiple organizations,
-# # but organizations cannot be shared
-# class Organization(models.Model):
-
-# 	name    = models.CharField(max_length=200)
-# 	email   = models.EmailField()
-# 	address = models.CharField(max_length=50)
-# 	country = models.CharField(max_length=50)
-# 	university = models.ForeignKey(University, on_delete=models.CASCADE)
-# 	# student    = models.ManyToManyField('Student')
-
-# 	def __str__(self):
-# 		return self.name
