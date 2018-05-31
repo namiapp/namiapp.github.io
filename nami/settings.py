81738641c81c656f9a7b3420031e6d616052493f
namiapp <fortyninechickenfingers@gmail.com>
2018-05-31 14:21:50 +0900

    Styled buttons on navbar and on Nami index page; abstracted HTML to base.html, added normalize.css, and dropped postgres database and recreated it without the custom user inheriting from abstractuser.

--- nami/settings.py
+++ nami/settings.py
@@ -128,7 +128,7 @@ LOGOUT_REDIRECT_URL = '/'
 EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
 EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
 
-AUTH_USER_MODEL = 'users.CustomUser'
+# AUTH_USER_MODEL = 'users.CustomUser'
 
 STATIC_URL = '/static/'
 
